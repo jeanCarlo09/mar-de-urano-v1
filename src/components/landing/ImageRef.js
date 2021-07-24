775d044b288c1fdee76843149cab308705204f39
Spectre09 <jean.vargas@ucrso.info>
2021-07-24 10:57:12 -0500

    test 2 Images loaded from cache don't trigger the onLoad event

--- src/components/landing/ImageRef.js
+++ src/components/landing/ImageRef.js
@@ -2,12 +2,13 @@ import React, { useEffect, useRef } from 'react';
 
 
 export const ImageRef = ({ ...props }) => {
-
+    // console.log(props);
     const ref = useRef(null);
 
     useEffect(() => {
         ref.current.complete &&
-            console.log('Cargado');
+            // console.log('Cargado');
+            props.onLoad();
     }, []);
 
     return (
